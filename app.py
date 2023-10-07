58fa40c8ce3486633b903272a5a9355056efdc86
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-10-06 23:11:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
 import streamlit as st
+import speech_recognition as sr
 
+# Barra lateral
 with st.sidebar:
-  st.subheader("Configura la modalidad")
-  mod_radio = st.radio(
-    "Escoge la modalidad a usar",
-    ("Visual", "Auditiva", "Haptica")
-  )
+    st.subheader("Configura la modalidad")
+    mod_radio = st.radio(
+        "Escoge la modalidad a usar",
+        ("Visual", "Auditiva", "Haptica")
+    )
+
+# Interfaz para la modalidad "Audio a Texto"
+if mod_radio == "Auditiva":
+    st.subheader("Audio a Texto")
+
+    # Crear un botón para que el doctor pida al paciente hablar
+    if st.button("Pídale al paciente que hable"):
+        recognizer = sr.Recognizer()
+
+        with sr.Microphone() as source:
+            st.info("Hable ahora...")
+            audio = recognizer.listen(source)
+
+        try:
+            text = recognizer.recognize_google(audio)
+            st.success("Texto convertido del audio:")
+            st.write(text)
+        except sr.UnknownValueError:
+            st.warning("No se pudo reconocer el audio.")
+        except sr.RequestError as e:
+            st.error(f"Error en la solicitud de reconocimiento de voz: {e}")
+
+# Interfaz para la identificación de la persona
+if mod_radio == "Visual":
+    st.subheader("Identificación de la Persona")
